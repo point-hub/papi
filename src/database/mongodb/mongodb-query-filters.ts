661da4a283e901080943fd54f8ee1a0a13aceb0d
Martien Dermawan Tanama <martiendt@gmail.com>
2026-01-14 11:25:52 +0700

    feat: add date range filter

--- src/database/mongodb/mongodb-query-filters.ts
+++ src/database/mongodb/mongodb-query-filters.ts
@@ -26,6 +26,19 @@ export const addExactFilter = (filters: Record<string, unknown>[], field: string
   }
 }
 
+export const addDateRangeFilter = (filters: Record<string, unknown>[], field: string, value_from?: string, value_to?: string) => {
+  if (value_from?.trim() && value_to?.trim()) {
+    const range: Record<string, unknown> = {}
+    if (value_from?.trim()) {
+      range['$gte'] = new Date(value_from?.trim())
+    }
+    if (value_to?.trim()) {
+      range['$lte'] = new Date(value_to?.trim())
+    }
+    filters.push({ [field]: range })
+  }
+}
+
 /**
  * Adds a numeric filter to the filters array based on comparison operators or a single value.
  *
@@ -130,5 +143,6 @@ export default {
   parseComparisons,
   addNumberFilter,
   addRegexFilter,
+  addDateRangeFilter,
   addExactFilter
 }
